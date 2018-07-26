52efce64fa8e3681635df56205594491acf28a0a
Vineet Bansal <vineetbansal@gmail.com>
2018-07-26 12:09:49 -0400

    DB Case Fix in settings.py for CI tests to pass.

--- ffmeta/settings.py
+++ ffmeta/settings.py
@@ -9,7 +9,7 @@ DB_USER = "travis"
 DB_PASS = ""
 DB_HOST = "localhost"
 DB_PORT = 3306
-DB_NAME = "ffmeta"
+DB_NAME = "FFMeta"
 
 # SQLAlchemy settings
 SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8".format(DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME)
